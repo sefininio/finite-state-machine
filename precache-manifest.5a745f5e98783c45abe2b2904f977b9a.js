54c50b49fb19544961a996acb2de5cfb9eaaf77c
Sefi Ninio <sefi.ninio@gmail.com>
2020-06-12 08:32:10 +0300

    Updates

---
+++ precache-manifest.5a745f5e98783c45abe2b2904f977b9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2c7fad1cbf91e55c8309383126f167c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "595f4c0d7303fa891f86",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fad144a4ff62dec528f1",
+    "url": "/static/js/2.e8f61821.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.e8f61821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "595f4c0d7303fa891f86",
+    "url": "/static/js/main.a38ea660.chunk.js"
+  },
+  {
+    "revision": "a704bebad5eaa411acd9",
+    "url": "/static/js/runtime-main.e4054d78.js"
+  }
+]);
